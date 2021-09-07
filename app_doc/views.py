d2eda66cb7900dfb8b3caa25f57d7b5698a22f96
zmister <zmister@qq.com>
2021-09-07 23:48:06 +0800

    优化文档名称的XSS过滤

--- app_doc/views.py
+++ app_doc/views.py
@@ -36,6 +36,53 @@ import hashlib
 import markdown
 
 
+# HTML转义
+def jsonXssFilter(data):
+    payloads = {
+        '\'':'&apos;',
+        '"':'&quot;',
+        '<':'&lt;',
+        '>':'&gt;'
+    }
+    if type(data) == dict:
+        new = {}
+        for key,values in data.items():
+            new[key] = jsonXssFilter(values)
+    elif type(data) == list:
+        new = []
+        for i in data:
+            new.append(jsonXssFilter(i))
+    elif type(data) == int or type(data) == float:
+        new = data
+    elif type(data) == str:
+        new = data
+        for key,value in payloads.items():
+            new = new.replace(key,value)
+    elif type(data) ==bytes:
+        new = data
+    else:
+        print('>>> unknown type:')
+        print(type(data))
+        new = data
+    return new
+
+
+def html_filter(data):
+    if len(data) == 0:
+        return ""
+    payloads = {
+        '\'':'&apos;',
+        '"':'&quot;',
+        '<':'&lt;',
+        '>':'&gt;'
+    }
+    new = data
+    for key, value in payloads.items():
+        new = new.replace(key, value)
+    print(new)
+    return new
+
+
 # 替换前端传来的非法字符
 def validateTitle(title):
   rstr = r"[\/\\\:\*\?\"\<\>\|\[\]]" # '/ \ : * ? " < > |'
@@ -2071,6 +2118,7 @@ def get_pro_doc_tree(request):
             # 如果一级文档没有下级文档，直接保存
             else:
                 doc_list.append(top_item)
+        doc_list = jsonXssFilter(doc_list)
         return JsonResponse({'status':True,'data':doc_list})
     else:
         return JsonResponse({'status':False,'data':_('参数错误')})
