6eae36ecca57287175055acc8d53215cee5cf598
zmister <zmister@qq.com>
2022-05-18 13:48:15 +0800

    优化docker启动脚本

--- docker_mrdoc.sh
+++ docker_mrdoc.sh
@@ -5,7 +5,7 @@ python /app/MrDoc/manage.py makemigrations &&
 # 根据数据库迁移文件执行数据库变更
 python /app/MrDoc/manage.py migrate &&
 # 重建全文搜索索引
-nohup echo y |python /app/MrDoc/manage.py rebuild_index &&
+nohup echo y |python /app/MrDoc/manage.py rebuild_index &
 # 启动uwsgi
 uwsgi --ini /app/MrDoc/config/uwsgi.ini
 # 直接 runserver 方式运行
