7dd78a7799fd3b1bd17eb6bbbe5062550be02e23
yangjian <zmister@qq.com>
2025-11-12 15:18:53 +0800

    feat:优化文集文档页面左侧文集目录大纲样式与专业版保持一致

--- static/mrdoc/mrdoc-docs.js
+++ static/mrdoc/mrdoc-docs.js
@@ -6,18 +6,78 @@
 
 // Ajax默认配置
 $.ajaxSetup({
-    data: {csrfmiddlewaretoken: '{{ csrf_token }}' },
+    data: {csrfmiddlewaretoken: csrf_token_str },
 });
+
+// 生成文集目录
+getProjectToc = function(tree){
+    var toc_str = ""
+    for (var i = 0; i < tree.length; i++) {
+        var item = tree[i];
+        toc_str += '<li class="doctree-li">'
+        if(item.children != undefined && item.children.length > 0){ // 存在下级文档
+            li = '<div class="doctree-item-wrapper"><span class="doctree-item-name-wrapper has-children-icon">'
+            if(item.open_children){
+                toc_switch_icon = '<i class="layui-icon layui-icon-down switch-toc"></i>'
+            }else{
+                toc_switch_icon = '<i class="layui-icon layui-icon-right switch-toc"></i>'
+            }
+            li += toc_switch_icon
+            if(item.editor_mode == 5){
+                toc_doc_link = '<a href="' + item.pre_content +'" class="doc-link" title="'+ item.pre_content +'">' + item.name + '</a>'
+            }else if(item.editor_mode == 4){
+                toc_doc_link = '<a href="/doc/'+ item.id +'/" class="doc-link"'  + 'data-id="' + item.id + '" title="'+ item.name +'">' + item.name + '</a>'
+            }else{
+                toc_doc_link = '<a href="/doc/'+ item.id +'/" class="doc-link"'  + 'data-id="' + item.id + '" title="'+ item.name +'">' + item.name + '</a>'
+            }
+            toc_doc_link += '</span>'
+            if(is_creater){
+                addLink = `<button class="doctree-item-btn" data-id="${item.id}"><i class="layui-icon layui-icon-more"></i></button>`
+                toc_doc_link += addLink
+            }
+            li += toc_doc_link
+            if(item.open_children){
+                sub_str = '</div><div class="sub-items visible"><ul class="doctree-ul">'
+            }else{
+                sub_str = '</div><div class="sub-items"><ul class="doctree-ul">'
+            }
+            li += sub_str
+            toc_str += li
+            toc_str += getProjectToc(item['children'])
+            toc_str += '</ul></div>'
+        }else{//不存在下级文档
+            li = '<div class="doctree-item-wrapper"><span class="doctree-item-name-wrapper no-children-icon">'
+            if(item.editor_mode == 5){
+                toc_doc_link = '<a href="'+ item.pre_content +'" class="doc-link" title="'+ item.pre_content +'"><i class="layui-icon layui-icon-link"></i>&nbsp;' + item.name + '</a>'
+            }else if(item.editor_mode == 4){
+                toc_doc_link = '<a href="/doc/'+ item.id +'/" class="doc-link"'  + 'data-id="' + item.id + '" title="'+ item.name +'"><i class="iconfont mrdoc-icon-table"></i>&nbsp;' + item.name + '</a>'
+            }else if(item.editor_mode == 6){
+                toc_doc_link = '<a href="/doc/'+ item.id +'/" class="doc-link"'  + 'data-id="' + item.id + '" title="'+ item.name +'"><i class="layui-icon layui-icon-release"></i>&nbsp;' + item.name + '</a>'
+            }else{
+                toc_doc_link = '<a href="/doc/'+ item.id +'/" class="doc-link"'  + 'data-id="' + item.id + '" title="'+ item.name +'"><i class="iconfont mrdoc-icon-wendang"></i>&nbsp;' + item.name + '</a>'
+            }
+            toc_doc_link += '</span>'
+            if(is_creater){
+                addLink = `<button class="doctree-item-btn" data-id="${item.id}"><i class="layui-icon layui-icon-more"></i></button>`
+                toc_doc_link += addLink
+            }
+            li += toc_doc_link
+            toc_str += li
+        }
+        toc_str += '</li>'
+    };
+    return toc_str;
+};
 
 // 视频iframe域名白名单
 var iframe_whitelist = '{{ iframe_whitelist }}'.split(',')
 
 //为当前页面的目录链接添加蓝色样式
 tagCurrentDoc = function(){
-    $("nav li a").each(function (i) {
+    $("nav li a.doc-link").each(function (i) {
         var $me = $(this);
         var lochref = $.trim(window.location.href); // 获取当前URL
-        var mehref = $.trim($me.get(0).href); 
+        var mehref = $.trim($me.get(0).href);
         if (lochref.indexOf(mehref) != -1) {
             // console.log($me,lochref,mehref)
             $me.closest("li").addClass("active");
@@ -25,26 +85,19 @@ tagCurrentDoc = function(){
             if($me.parent().next("ul.sub-menu").hasClass("toc-close")){
                 // console.log("展开下级目录")
                 $me.parent().next("ul.sub-menu").toggleClass("toc-close");
-                $me.next("i").toggleClass("layui-icon-left layui-icon-down");
+                $me.prevAll("i:first").toggleClass("layui-icon-right layui-icon-down"); // 切换当前文档的图标
             }
-            // 展开当前文档的所有上级目录
-            if($me.parent("li").parent('ul.sub-menu').hasClass("toc-close")){
-                // console.log("展开二级文档目录")
-                $me.parent("li").parent('ul.sub-menu').toggleClass("toc-close");
-            }
-            if($me.parent("div").parent('li').parent('ul.sub-menu').hasClass("toc-close")){
-                // console.log("展开包含下级的二级文档目录")
-                $me.parent("div").parent('li').parent('ul.sub-menu').toggleClass("toc-close");
-            }
-            if($me.parent("li").parent('ul').parent('li').parent('ul.sub-menu').hasClass("toc-close")){
-                // console.log("展开三级目录")
-                $me.parent("li").parent('ul').parent('li').parent('ul.sub-menu').toggleClass("toc-close");
-            }
+            // 展开当前文档的所有层级上级目录
+            $me.parents("ul.sub-menu").each(function(index,elem){
+               if($(elem).hasClass("toc-close")){
+                    $(elem).toggleClass("toc-close")
+               };
+            });
             // 切换图标
             $me.parents("ul.sub-menu").prevAll("div").each(function(i){
                 var $link = $(this);
-                if($link.children("i").hasClass("layui-icon-left")){
-                    $link.children("i").toggleClass("layui-icon-left layui-icon-down");
+                if($link.children("i").hasClass("layui-icon-right")){
+                    $link.children("i").toggleClass("layui-icon-right layui-icon-down");
                 }
             })
             // 目录的当前文档滚动于目录顶端
@@ -55,7 +108,103 @@ tagCurrentDoc = function(){
         }
     });
 };
-tagCurrentDoc();
+// tagCurrentDoc();
+
+activeCurrentDoc = function(){
+    // 获取当前页面的路径
+    const currentPath = window.location.pathname;
+    // 使用正则表达式提取数字部分
+    let match;
+    if( currentPath.startsWith('/share_project/')){
+        match = currentPath.match(/\/share_project\/.*?\/(\w+)(\/.*)?$/);
+    }else{
+        match = currentPath.match(/\/doc\/(\w+)(\/.*)?$/);
+    }
+
+    // 检查是否有匹配结果
+    if (match) {
+    // 获取提取的数字部分
+        const docId = match[1];
+        // console.log("提取到的文档ID:", docId);
+        $('.doc-link').each(function() {
+            const btnDataId = $(this).data('id');
+            const parentLi = $(this).closest('.doctree-li');
+            const parentItem = $(this).closest('.doctree-item-wrapper');
+            // console.log(btnDataId)
+            // 检查是否与当前文档ID匹配
+            if (btnDataId == docId) {
+                // 1. 为按钮所在的文档名称div添加active类名
+                parentItem.addClass('active');
+
+                // 2. 展开当前文档的下级目录
+                if(!parentLi.find(".sub-items").first().hasClass("visible")){
+                    // console.log("展开下级目录")
+                    parentLi.find(".sub-items").first().addClass("visible");
+                }
+                // 3. 展开当前文档的图标
+                if(!parentItem.find('.switch-toc').first().hasClass('.layui-icon-down')){
+                    parentItem.find('.switch-toc').first().removeClass('layui-icon-right').addClass('layui-icon-down')
+                }
+
+                // 4. 展开所有上级
+                $(this).parents("div.sub-items").each(function(index,elem){
+                    if(!$(elem).hasClass("visible")){
+                         $(elem).addClass("visible")
+                    };
+                 });
+
+                 // 5. 展开所有上级图标
+                $(this).parents("div.sub-items").prev('.doctree-item-wrapper').each(function(index,elem){
+                    if(!$(elem).find('.switch-toc').first().hasClass('.layui-icon-down')){
+                        $(elem).find('.switch-toc').first().removeClass('layui-icon-right').addClass('layui-icon-down');
+                    }
+                 });
+
+                 // 滚动当前文档于可视范围内
+                 setTimeout(function() {
+                    document.querySelector(`[data-id="${docId}"]`).scrollIntoView({ behavior: 'auto', block: "start" });
+                }, 50);
+            }
+        });
+    }
+};
+activeCurrentDoc();
+
+// 复制文本到剪贴板
+function copyToClipboard(text) {
+    // 判断是否支持 Clipboard API
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text)
+        .then(function() {
+          console.log('文本已成功复制到剪贴板:', text);
+          layer.msg("文档链接已复制到剪贴板")
+        })
+        .catch(function(err) {
+          console.error('无法复制文本到剪贴板:', err);
+        });
+    } else {
+      // 如果不支持 Clipboard API，则使用 document.execCommand('copy')
+      var textarea = document.createElement('textarea');
+      textarea.value = text;
+      textarea.style.position = 'fixed';
+      document.body.appendChild(textarea);
+      textarea.select();
+
+      try {
+        var success = document.execCommand('copy');
+        if (success) {
+          console.log('文本已成功复制到剪贴板:', text);
+          layer.msg("文档链接已复制到剪贴板")
+        } else {
+          console.error('复制失败');
+        }
+      } catch (err) {
+        console.error('无法执行复制操作:', err);
+      } finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  }
 
 /*
     小屏幕下的文集大纲显示处理
@@ -86,7 +235,7 @@ function changeSidebar(){
     }
 };
 // 监听文档div点击
-document.querySelector('.doc-body').addEventListener('click', function (e) {
+document.getElementById('doc-container-body').addEventListener('click', function (e) {
     var screen_width = window.matchMedia('(max-width: 768px)');
     // 小屏下收起左侧文集大纲
     if(screen_width.matches){
@@ -95,7 +244,7 @@ document.querySelector('.doc-body').addEventListener('click', function (e) {
     }
 });
 
-/* 
+/*
     切换隐藏侧边栏
 */
 // 初始化左侧文集大纲状态
@@ -118,27 +267,27 @@ function init_sidebar(){
             }else{
                 window.localStorage.setItem('bgpage','0')
             }
-            
+
         }
     }
-    
+
 }
 init_sidebar();
 // 切换侧边栏
 $(function(){
     $(".js-toolbar-action").click(toggleSidebar);
 });
-//切换侧边栏显示隐藏
+// 切换侧边栏显示隐藏
 function toggleSidebar(){
     console.log("切换侧边栏")
     $("body").toggleClass("big-page");
     if(window.localStorage.getItem('bgpage') === '1'){
-        window.localStorage.setItem('bgpage','0')
+        window.localStorage.setItem('bgpage','0');
         if(doc_editor_mode == '4'){
             luckysheet.resize();
         }
     }else{
-        window.localStorage.setItem('bgpage','1')
+        window.localStorage.setItem('bgpage','1');
         if(doc_editor_mode == '4'){
             luckysheet.resize();
         }
@@ -157,8 +306,8 @@ initTheme = function(){
     // 如果本地存储为夜间模式
     if(themeDarkStatus == '1' && $("html").hasClass("theme-dark") == false){
         $("html").toggleClass("theme-dark")
-        $(".theme-switch i").removeClass("fa-moon-o")
-        $(".theme-switch i").addClass("fa-sun-o")
+        $(".theme-switch i").removeClass("mrdoc-icon-night")
+        $(".theme-switch i").addClass("mrdoc-icon-light")
     }
     console.log(darkmode.isActivated())
     if(darkmode.isActivated()){
@@ -173,13 +322,13 @@ $(function(){
 function toggleDark(){
     if($("html").hasClass("theme-dark")){
         window.localStorage.removeItem("theme-dark")
-        $(".theme-switch i").removeClass("fa-sun-o")
-        $(".theme-switch i").addClass("fa-moon-o")
+        $(".theme-switch i").removeClass("mrdoc-icon-light")
+        $(".theme-switch i").addClass("mrdoc-icon-night")
         $("a.theme-switch").attr("title","切换至夜间模式")
     }else{
         window.localStorage.setItem("theme-dark","1")
-        $(".theme-switch i").removeClass("fa-moon-o")
-        $(".theme-switch i").addClass("fa-sun-o")
+        $(".theme-switch i").removeClass("mrdoc-icon-night")
+        $(".theme-switch i").addClass("mrdoc-icon-light")
         $("a.theme-switch").attr("title","切换至日间模式")
     }
     $("html").toggleClass("theme-dark");
@@ -188,6 +337,9 @@ function toggleDark(){
         darkmode.toggle();
     }
 }
+
+
+
 
 /*
     页面初始化字体设置
@@ -211,20 +363,20 @@ if(window.localStorage.getItem('font-size')){// 字体大小
 $(document).ready(function() {
     // 初始时，“返回顶部”标签隐藏
     $(".toTop").hide();
-    $(".editDoc").hide();
-    $(window).scroll(function() {
+    // $(".editDoc").hide();
+    $("#doc-container-body").scroll(function() {
         // 若滚动的高度，超出指定的高度后，“返回顶部”的标签出现。
-        if($(document).scrollTop() >= 140) {
+        if($("#doc-container-body").scrollTop() >= 140) {
             $(".toTop").show();
-            $(".editDoc").show();
+            // $(".editDoc").show();
         } else {
             $(".toTop").hide();
-            $(".editDoc").hide();
+            // $(".editDoc").hide();
         }
     })
     // 绑定点击事件，实现返回顶部的效果
     $(".toTop").click(function() {
-        $(document).scrollTop(0);
+        $("#doc-container-body").scrollTop(0);
     });
     // 生成当前网页链接
     $("input[name=current_url]").val(document.URL)
@@ -296,17 +448,12 @@ $("#dashang").click(function(r){
 /*
     右侧文档目录
 */
-$(function(){
-    // $(".switch-toc").click(SwitchToc);
-    $("body").on('click','.switch-toc',SwitchToc);
-});
 // 切换文档目录
-function SwitchToc(i){
-    // console.log("点击了")
-    var $me = $(this);
-    $(this).parent().next("ul").toggleClass("toc-close"); //切换展开收起样式
-    $(this).toggleClass("layui-icon-left layui-icon-down");//切换图标
-};
+$(document).on('click', '.switch-toc', function () {
+    $(this).toggleClass("layui-icon-right layui-icon-down");//切换图标
+    var parentLi = $(this).closest('.doctree-li');
+    parentLi.find('.sub-items').first().toggleClass('visible');
+  });
 
 // $(".switch-toc-div").click(function(e){
 //     console.log(e)
@@ -318,16 +465,16 @@ function openDocTree(){
     $("nav ul.summary ul").each(function(obj){
         console.log(obj,this)
         $(this).removeClass("toc-close")
-        $(this).prev().children('i').toggleClass("layui-icon-left layui-icon-down");//切换图标
+        $(this).prev().children('i').toggleClass("layui-icon-right layui-icon-down");//切换图标
     })
-    
+
 };
 // 收起文档树
 function closeDocTree(){
     $("nav ul.summary ul").each(function(obj){
         console.log(obj,this)
         $(this).addClass("toc-close")
-        $(this).prev().children('i').toggleClass("layui-icon-left layui-icon-down");//切换图标
+        $(this).prev().children('i').toggleClass("layui-icon-right layui-icon-down");//切换图标
     })
 };
 
@@ -371,28 +518,35 @@ doc_qrcode = function(){
 };
 doc_qrcode();
 
-/* 
+/*
     文集水印
 */
-textBecomeImg = function(text,fontsize,fontcolor){
-    var canvas = document.createElement('canvas');
-    canvas.height = 180;
-    canvas.width = 400;
-    var context = canvas.getContext('2d');
-    // 擦除(0,0)位置大小为200x200的矩形，擦除的意思是把该区域变为透明
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = fontcolor;
-    context.font=fontsize+"px Arial";
-    context.rotate(-25 * Math.PI / 180)
-    context.fillText(text,10,160);
-    
-    var dataUrl = canvas.toDataURL('image/png');//注意这里背景透明的话，需要使用png
-    return dataUrl;
+function svgToBase64(svg) {
+    return 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svg)));
 }
 
-function initWhterMark(value){
-    var img_base64 = textBecomeImg(value, '14', '#000');
-    document.getElementById("wm").style.background = 'url('+ img_base64 + ')';
+function createSvg(text, fontSize = 14, opacity = 0.15) {
+    const svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" style="transform: rotate(-15deg); transform-origin: 50% 50%;">
+        <text
+        x="50%" y="50%"
+        dominant-baseline="middle" text-anchor="middle"
+        fill="rgba(156, 162, 169, ${opacity})"
+        font-family="'PingFang SC', 'Microsoft YaHei', Arial, sans-serif"
+        font-size="${fontSize}px">
+        ${text}
+        </text>
+    </svg>`;
+    return svgToBase64(svg);
+}
+
+function initWhterMark(text) {
+    const wm = document.getElementById('wm');
+    const svg1 = createSvg(text);
+    const svg2 = createSvg(text);
+
+    wm.style.backgroundImage = `url('${svg1}'), url('${svg2}')`;
+    wm.style.backgroundPosition = `109px 109px, 0 0`;
 }
 
 // 文集、文档收藏函数
@@ -445,19 +599,20 @@ function initDocRender(mode){
             id:'content',
             value:$("#content textarea").val(),
             cdn:"/static/mr-marked/",
-//            hljsLineNumber:code_line_number ? true : false,
+            hljsLineNumber:code_line_number ? true : false,
         })
         marked.renderGraphic()
         var docToc = marked.getToc();
         $("#toc-container").append(docToc);
     }else if(mode == 2){
         var md_content = $("#content textarea").val()
-        Vditor.preview(document.getElementById('content'),md_content, 
+        Vditor.preview(document.getElementById('content'),md_content,
         {
-            "cdn":"/static/vditor/",
+            "cdn":"/static/vditor",
             markdown:{mark:true},
             speech: {enable: true,},
             anchor: 1,
+            hljs:{lineNumber:code_line_number ? true : false},
             after () {
                 var sub_ele = "<div class='markdown-toc editormd-markdown-toc'></div>"
                 $("#toc-container").append(sub_ele)
@@ -467,11 +622,6 @@ function initDocRender(mode){
                 if (outlineElement[0].innerText.trim() !== '') {
                     outlineElement[0].style.display = 'block';
                     var toc_cnt = $(".markdown-toc-list ul").children().length;
-                    // if(toc_cnt > 0){
-                        //console.log('显示文档目录')
-                        // $(".tocMenu").show();
-                        // initSidebar('.sidebar', '.doc-content', 2);
-                    // }
                 }
                 // 图片放大显示
                 var img_options = {
@@ -490,10 +640,12 @@ function initDocRender(mode){
                 if(toc_cnt > 0){
                     // console.log('显示文档目录')
                     $(".tocMenu").show();
-                    initSidebar('.sidebar', '.doc-content');
+                    initSidebar('.sidebar', '.doc-content', mode=2);
                 };
                 // 高亮搜索词
                 keyLight('doc-content',getQueryVariable("highlight"));
+                // 跳转到搜索词
+                scrollIntoKey('doc-content',getQueryVariable("highlight"));
             },
         })
     }else if(mode == 4){
@@ -511,7 +663,7 @@ function initDocRender(mode){
             userInfo: false, // 右上角的用户信息展示样式
             enableAddRow:false, // 允许添加行
             enableAddBackTop:false, // 回到顶部
-            
+
             // plugins: ['chart'],
             showstatisticBarConfig: {
                 count:false,
@@ -551,3 +703,28 @@ function keyLight(id, key, bgColor){
         markInstance.mark(key);
     }
 };
+
+// 搜索词跳转
+function scrollIntoKey(id,key){
+    var searchRegex = new RegExp(decodeURIComponent(key), 'iu');
+    var elements = document.getElementById(id).querySelectorAll("h1,h2,h3,h4,h5,h6,p,a");
+    // 遍历元素列表
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        // 检查元素的文本内容是否包含搜索的文本
+        if (searchRegex.test(element.textContent)) {
+            // console.log(element)
+            // 将匹配的元素滚动到可视区域
+            element.scrollIntoView({ block: 'start' });
+            // 找到第一个匹配后退出循环
+            break;
+        }
+    }
+}
+
+// 判断是否为微信内置浏览器
+function isWeChatBrowser() {
+    // alert(navigator.userAgent)
+    const ua = navigator.userAgent.toLowerCase();
+    return ua.includes('micromessenger') && !ua.includes('wxwork');
+}
