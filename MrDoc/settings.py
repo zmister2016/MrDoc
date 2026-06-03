d38c3972aaaab44819de3ac05717f1506d94460e
yangjian <zmister@qq.com>
2026-06-03 10:53:02 +0800

    v1.0.0

--- MrDoc/settings.py
+++ MrDoc/settings.py
@@ -51,7 +51,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = CONFIG.getboolean('site','debug',fallback=False)
 
-VERSIONS = '0.9.9'
+VERSIONS = '1.0.0'
 
 ALLOWED_HOSTS = ['*']
 
